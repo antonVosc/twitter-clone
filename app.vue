aaa087f40727d6639f0747a6609c0fe540924c08
antonVosc <anton.vosc@gmail.com>
2024-12-29 00:08:46 -0400

    Working with app logo

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <h1 class="text-red-500">Twitter clone</h1>
+  <div :class="{'dark' : darkMode}">
+    <div class="bg-white dark:bg-dim-900">
+      <div clas="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div classs="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+  const darkMode = ref(false)
+</script>
