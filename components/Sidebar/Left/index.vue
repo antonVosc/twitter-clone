4c9f448833d34b157f881ce62c8311a2ac081051
antonVosc <anton.vosc@gmail.com>
2025-01-04 19:44:12 +0000

    Testing project file location change

--- components/Sidebar/Left/index.vue
+++ components/Sidebar/Left/index.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="flex flex-col h-screen">
-        <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="transition">
+        <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20" :class="defaultTransition">
             <nuxt-link to="/">
                 <div class="w-8 h-8">
                     <LogoTwitter />
                 </div>
             </nuxt-link>
         </div>
+        
+        <HomeIcon />
     </div>
 </template>
 
 <script setup>
-    const transition = 'transition ease-in-out duration-350'
+    import { HomeIcon } from "@heroicons/vue/solid"
+    const { defaultTransition } = useTailwindConfig()
 </script>
